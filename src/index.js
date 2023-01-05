fa9ea10d7eda746e230f3559ed022e923370385c
Denys Volodko <babyleejane@gmail.com>
2023-01-05 17:29:32 +0200

    fetch done

--- src/index.js
+++ src/index.js
@@ -1,31 +1,63 @@
 // Зареєструватись на піксабей і отримати свій ключ ✔️
 // Зробити запит на піксабей і отримати картинку ✔️
 // Зробити запит і отримати масив картинок ✔️
-// Створити функцію рендерингу результату фетча на сторінці
-// Створити фронтент з інпутом і сабміт кнопкою
+// Створити функцію рендерингу результату фетча на сторінці ✔️
+// Створити фронтент з інпутом і сабміт кнопкою ✔️
+// створити CSS стилізацію сторінки
 // додати пагінацію
 // додати нескінчений скрол
 // додати нотіфлікс повідомлення
 // додати сімпллайтбокс галерею
 
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import ApiService from './js/api-service';
 
 const refs = {
-  searchForm: document.querySelector(''),
+  searchForm: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreButton: document.querySelector('.load-more'),
 };
 
-const axios = require('axios').default;
-const API_KEY = '32552782-0d4c86680018457e820f20492';
+const apiService = new ApiService();
 
-async function getPicture() {
-  try {
-    const response = await axios.get(
-      `https://pixabay.com/api/?key=${API_KEY}&q=yellow+flowers&image_type=photo&per_page=50`
-    );
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreButton.addEventListener('click', onLoadMore);
+
+async function onSearch(e) {
+  e.preventDefault();
+  apiService.query = e.currentTarget.elements.searchQuery.value.trim();
+  apiService.resetPage();
+  const pictures = await apiService.getPictures();
+  renderCards(pictures.data.hits);
 }
 
-getPicture();
+async function onLoadMore() {
+  const pictures = await apiService.getPictures();
+  console.log(pictures);
+}
+
+function renderCards(pictureCards) {
+  console.log(pictureCards);
+  const markup = pictureCards
+    .map(
+      pictureCard => `<div class="photo-card">
+    <img src="${pictureCard.webformatURL}" alt="${pictureCard.tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes: ${pictureCard.likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views: ${pictureCard.views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments: ${pictureCard.comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads: ${pictureCard.downloads}</b>
+      </p>
+    </div>
+  </div>`
+    )
+    .join('');
+  refs.gallery.innerHTML = markup;
+}
---
+++ src/js/api-service.js
@@ -0,0 +1,34 @@
+const axios = require('axios').default;
+const API_KEY = '32552782-0d4c86680018457e820f20492';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  async getPictures() {
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`
+    );
+    this.incrementPage();
+    return response;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
